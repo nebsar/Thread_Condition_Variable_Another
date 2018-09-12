64c6c31729900b6e3cecf829cf1058f4a092d9ed
Eagleye <Eagleye@EagleEye>
2018-09-11 23:48:29 -0400

    Thread_Condition_Variable_Another

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=thread_condition_variable_another
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/thread_condition_variable_another
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=threadconditionvariableanother.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/threadconditionvariableanother.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=thread_condition_variable_another
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/thread_condition_variable_another
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=threadconditionvariableanother.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/threadconditionvariableanother.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
